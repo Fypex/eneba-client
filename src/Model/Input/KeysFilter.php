0a6ff4a4bbff2aff28e8c79a1d26faecbd8d2f70
Maksim Grishchenko <endtimes104@gmail.com>
2021-09-16 13:50:37 +0300

    Update Keys Filter

--- src/Model/Input/KeysFilter.php
+++ src/Model/Input/KeysFilter.php
@@ -11,14 +11,26 @@ class KeysFilter extends PaginationFilter
      * @var KeyStateEnum|null
      */
     protected $state;
+    protected $ids = [];
 
     public function getState(): ?KeyStateEnum
     {
         return $this->state;
     }
 
+    public function getIds()
+    {
+        return $this->ids;
+    }
+
     public function setState(?KeyStateEnum $state): void
     {
         $this->state = $state;
     }
+
+    public function setIds(array $ids)
+    {
+        $this->ids = $ids;
+    }
+
 }
